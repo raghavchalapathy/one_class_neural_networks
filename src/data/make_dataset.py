60b693847dd40c88418e55db43bf4f8aeda77bba
raghav <raghav.chalapathy@gmail.com>
2018-08-12 11:57:32 +1000

    lHC and other experiments

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,29 +1,97 @@
 from sklearn.preprocessing import StandardScaler
 import numpy as np
+import json
 
 class CreateDataSet:
-    
+
     dataPath = '../data/raw/'
     scaler = StandardScaler()
 
+    def load_LHCDataset(self,data_dir_path):
 
-    def get_TestingData(self):
+        background = np.array([
+            json.loads(s)
+            for s in open(data_dir_path + '/bgimages_njge3_100k.dat')
+        ])
+        signal = np.array([
+            json.loads(s)
+            for s in open(data_dir_path + '/sigimages_njge3_100k.dat')
+        ])
 
+        return [background, signal]
+
+    def get_LHC_TrainingData(self, normal, NUM_OF_NORMAL,
+                             NUM_OF_ANOMALIES):
+
+        normal = np.reshape(normal,(len(normal),1369))
         
+
+        StandardScaler(copy=True, with_mean=True, with_std=True)
+        # self.scaler.fit(normal)
+        # normal = self.scaler.transform(normal)
+     
+
+        SEED = 42
+        NUM_OF_NORMAL_DATA_INSTANCES_TRAIN = NUM_OF_NORMAL
+        NUM_OF_ANOMALOUS_DATA_INSTANCES = NUM_OF_ANOMALIES
+
+        np.random.shuffle(normal)
+        train_normal = normal[0:NUM_OF_NORMAL_DATA_INSTANCES_TRAIN]
+        
+        # print("Unique train_normal:train_normal", np.unique(train_normal))
+        label_normal = 1 * np.ones(len(train_normal))
+
+        return [train_normal, label_normal]
+
+    def get_LHC_TestingData(self, normal, anomalies, NUM_OF_NORMAL,
+                            NUM_OF_ANOMALIES):
+
+        normal = np.reshape(normal,(len(normal),1369))
+        anomalies = np.reshape(normal,(len(anomalies),1369))
+
+        StandardScaler(copy=True, with_mean=True, with_std=True)
+        # self.scaler.fit(normal)
+        # normal = self.scaler.transform(normal)
+        # self.scaler.fit(anomalies)
+        # anomalies = self.scaler.transform(anomalies)
+        
+        SEED = 42
+        NUM_OF_NORMAL_DATA_INSTANCES_TRAIN = NUM_OF_NORMAL
+        NUM_OF_ANOMALOUS_DATA_INSTANCES = NUM_OF_ANOMALIES
+
+        np.random.shuffle(normal)
+        np.random.shuffle(anomalies)
+
+        test_normal = normal[NUM_OF_NORMAL:NUM_OF_NORMAL + 5000]
+        test_anomalies = anomalies[0:NUM_OF_ANOMALIES]
+
+       
+
+        # print("Unique test Normal Samples :test_normal",
+        #       np.unique(test_normal))
+        # print("Unique test Anomalous Samples :test_anomalies",
+        #       np.unique(test_anomalies))
+        label_normal = 1 * np.ones(len(test_normal))
+        label_anomalies = 0 * np.zeros(len(test_anomalies))
+
+        return [test_normal, label_normal, test_anomalies, label_anomalies]
+
+    def get_USPS_TestingData(self):
+
         import tempfile
         import pickle
-       
-        with open(self.dataPath+'usps_data.pkl','rb') as fp:
-              loaded_data1 = pickle.load(fp, encoding='latin1')
+
+        with open(self.dataPath + 'usps_data.pkl', 'rb') as fp:
+            loaded_data1 = pickle.load(fp, encoding='latin1')
 
         labels = loaded_data1['target']
         data = loaded_data1['data']
-        
-        ## Scale the data 
-        print(self.scaler.fit(data))
+
+        # ## Scale the data
+        # print(self.scaler.fit(data))
         StandardScaler(copy=True, with_mean=True, with_std=True)
-        data = self.scaler.transform(data)
-   
+        # data = self.scaler.transform(data)
+
         ## Select Ones
         k_ones = np.where(labels == 2)
         label_ones = labels[k_ones]
@@ -33,35 +101,30 @@ class CreateDataSet:
         label_sevens = labels[k_sevens]
         data_sevens = data[k_sevens]
 
+        data_ones = data_ones[220:440]
+        data_sevens = data_sevens[0:11]
 
-        data_ones = data_ones[220:440] 
-        data_sevens = data_sevens[0:11] 
+        label_ones = 1 * np.ones(len(data_ones))
+        label_sevens = np.zeros(len(data_sevens))
 
-        
-        label_ones      =  1*np.ones(len(data_ones))
-        label_sevens    =  np.zeros(len(data_sevens))
-        
+        return [data_ones, label_ones, data_sevens, label_sevens]
 
+    def get_USPS_TestingData_With_Random_UniformNoise(self):
 
-        return [data_ones,label_ones,data_sevens,label_sevens]
-
-    def get_TestingData_With_Random_UniformNoise(self):
-
-        
         import tempfile
         import pickle
-       
-        with open(self.dataPath+'usps_data.pkl','rb') as fp:
-              loaded_data1 = pickle.load(fp, encoding='latin1')
+
+        with open(self.dataPath + 'usps_data.pkl', 'rb') as fp:
+            loaded_data1 = pickle.load(fp, encoding='latin1')
 
         labels = loaded_data1['target']
         data = loaded_data1['data']
-        
-        ## Scale the data 
-        print(self.scaler.fit(data))
+
+        # ## Scale the data
+        # print(self.scaler.fit(data))
         StandardScaler(copy=True, with_mean=True, with_std=True)
-        data = self.scaler.transform(data)
-   
+        # data = self.scaler.transform(data)
+
         ## Select Ones
         k_ones = np.where(labels == 2)
         label_ones = labels[k_ones]
@@ -71,37 +134,31 @@ class CreateDataSet:
         label_sevens = labels[k_sevens]
         data_sevens = data[k_sevens]
 
+        data_ones = data_ones[220:440]
+        data_sevens = np.random.uniform(0, 1, (len(data_ones), 256))
 
-        data_ones = data_ones[220:440] 
-        data_sevens =  np.random.uniform(0,1,(len(data_ones),256))
+        label_ones = 1 * np.ones(len(data_ones))
+        label_sevens = np.zeros(len(data_sevens))
 
-        
-        label_ones      =  1*np.ones(len(data_ones))
-        label_sevens    =  np.zeros(len(data_sevens))
-        
+        return [data_ones, label_ones, data_sevens, label_sevens]
 
+    def get_USPS_TrainingData(self):
 
-        return [data_ones,label_ones,data_sevens,label_sevens]
-
-    def get_TrainingData(self):
-        
-        
         import tempfile
         import pickle
-       
 
-        with open(self.dataPath+'usps_data.pkl','rb') as fp:
-              loaded_data1 = pickle.load(fp, encoding='latin1')
+        with open(self.dataPath + 'usps_data.pkl', 'rb') as fp:
+            loaded_data1 = pickle.load(fp, encoding='latin1')
 
         labels = loaded_data1['target']
         data = loaded_data1['data']
-        
-        ## Scale the data 
-       
+
+        ## Scale the data
+
         print(self.scaler.fit(data))
         StandardScaler(copy=True, with_mean=True, with_std=True)
         data = self.scaler.transform(data)
-   
+
         ## Select Ones
         k_ones = np.where(labels == 2)
         label_ones = labels[k_ones]
@@ -111,14 +168,19 @@ class CreateDataSet:
         label_sevens = labels[k_sevens]
         data_sevens = data[k_sevens]
 
+        data_ones = data_ones[:220]
+        label_ones = 1 * np.ones(len(data_ones))
 
-        data_ones = data_ones[:220] 
-        label_ones      =  1*np.ones(len(data_ones))
-       
-        return [data_ones,label_ones] 
+        return [data_ones, label_ones]
+
+    def get_FAKE_Noise_TrainingData(self, X):
+
+        data_noise = np.random.uniform(0, 1, (len(X), 256))
+        label_noise = np.zeros(len(data_noise))
+        return [data_noise, label_noise]
     
-    def get_FAKE_Noise_TrainingData(self,X):
-        
-        data_noise =  np.random.uniform(0,1,(len(X),256))
-        label_noise    =  np.zeros(len(data_noise))
-        return [data_noise,label_noise]
+    def get_FAKE_Noise_LHC_TrainingData(self, X):
+
+        data_noise = np.random.uniform(0, 1, (len(X), 1369))
+        label_noise = np.zeros(len(data_noise))
+        return [data_noise, label_noise]
--- src/models/fakeNoiseNN.py
+++ src/models/fakeNoiseNN.py
@@ -27,13 +27,17 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_val_score
 from sklearn.externals import joblib
 from sklearn.base import BaseEstimator, ClassifierMixin
+from keras.layers import Activation
+from keras import backend as K
+from keras.utils.generic_utils import get_custom_objects
+import tensorflow as tf
 
 
-
-class FakeNoiseNN(BaseEstimator, ClassifierMixin):  
+class FakeNoiseNN(BaseEstimator, ClassifierMixin):
     """An example of classifier"""
 
-    def __init__(self, intValue=0, stringParam="defaultValue", otherParam=None):
+    def __init__(self, intValue=0, stringParam="defaultValue",
+                 otherParam=None):
         """
         Called when initializing the classifier
         """
@@ -41,75 +45,88 @@ class FakeNoiseNN(BaseEstimator, ClassifierMixin):
         self.stringParam = stringParam
 
         # THIS IS WRONG! Parameters should have same name as attributes
-        self.differentParam = otherParam 
-               
-        self.directory = "../models/supervisedBC/"
+        self.differentParam = otherParam
 
-    def train_KerasBinaryClassifier(self,X_train,y_train):
+        self.directory = "../models/supervisedBC/"
+        self.h_size = 64
+
+    def train_KerasBinaryClassifier(self, X_train, y_train,noOfepochs):
 
         # Use Tenserflow backend
         sess = tf.Session()
         K.set_session(sess)
 
+        def custom_activation(x):
+            return (1 / np.sqrt(self.h_size)) * tf.cos(x / 0.02)
+
+        get_custom_objects().update({
+            'custom_activation':
+            Activation(custom_activation)
+        })
+
         def model():
-            model = models.Sequential([
-                layers.Dense(128, input_dim=X_train.shape[1], activation='relu'),
-                layers.Dropout(0.5),
-                layers.Dense(1, activation='sigmoid')
-            ])
-            model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+            model = Sequential()
+            model.add(Dense(128, input_dim=X_train.shape[1]))
+            model.add(Activation('relu'))
+            model.add(Dense(64, activation='sigmoid'))
+            model.add(Dense(1))
+            model.compile(
+                optimizer='rmsprop',
+                loss='binary_crossentropy',
+                metrics=['accuracy'])
+
             return model
 
+        # early_stopping = callbacks.EarlyStopping(
+        #     monitor='val_loss', patience=1, verbose=0, mode='auto')
+        print("Removed Early stopping......")
+        pipe = pipeline.Pipeline([('rescale', preprocessing.StandardScaler()),
+                                  ('nn',
+                                   KerasClassifier(
+                                       build_fn=model,
+                                       epochs=noOfepochs,
+                                       batch_size=128,
+                                       validation_split=0.2))])
 
-        early_stopping = callbacks.EarlyStopping(monitor='val_loss', patience=1, verbose=0, mode='auto')
-
-        pipe = pipeline.Pipeline([
-            ('rescale', preprocessing.StandardScaler()),
-            ('nn', KerasClassifier(build_fn=model, nb_epoch=10, batch_size=128,
-                                   validation_split=0.2, callbacks=[early_stopping]))
-        ])
-
-
+                                    #    callbacks=[early_stopping]
         pipe.fit(X_train, y_train)
 
- 
         model_step = pipe.steps.pop(-1)[1]
         joblib.dump(pipe, os.path.join(self.directory, 'pipeline.pkl'))
-        print("Trained Model is Saved at relative path inside PROJECT_DIR ",self.directory)
-        models.save_model(model_step.model, os.path.join(self.directory, 'model.h5'))
+        print("Trained Model is Saved at relative path inside PROJECT_DIR ",
+              self.directory)
+        models.save_model(model_step.model,
+                          os.path.join(self.directory, 'model.h5'))
         return
 
-    def fit(self,X_Pos,X_PosLabel,X_Neg,X_NegLabel):
-        
-        data = np.concatenate((X_Pos,X_Neg),axis=0)
-        label = np.concatenate((X_PosLabel,X_NegLabel),axis=0)
-        print("Training the Keras Binary classifier.....")
-        self.train_KerasBinaryClassifier(data,label)
- 
-    def predict(self,X_testPos,X_testNeg):
-        
+    def fit(self, X_Pos, X_PosLabel, X_Neg, X_NegLabel,epochs):
 
-        X_test = np.concatenate((X_testPos,X_testNeg),axis=0)
+        data = np.concatenate((X_Pos, X_Neg), axis=0)
+        label = np.concatenate((X_PosLabel, X_NegLabel), axis=0)
+        print("Training the Keras Binary classifier.....")
+        self.train_KerasBinaryClassifier(data, label,epochs)
+
+    def predict(self, X_testPos, X_testNeg):
+
+        X_test = np.concatenate((X_testPos, X_testNeg), axis=0)
         X_testPosLabel = np.ones(len(X_testPos))
         X_testNegLabel = np.zeros(len(X_testNeg))
-        y_test = np.concatenate((X_testPosLabel,X_testNegLabel),axis=0)
+        y_test = np.concatenate((X_testPosLabel, X_testNegLabel), axis=0)
         pipe = joblib.load(os.path.join(self.directory, 'pipeline.pkl'))
         model = models.load_model(os.path.join(self.directory, 'model.h5'))
         pipe.steps.append(('nn', model))
 
-
         y_pred_keras = pipe.predict_proba(X_test)[:, 0]
         from sklearn.metrics import roc_curve
-        fpr_keras, tpr_keras, thresholds_keras = roc_curve(y_test, y_pred_keras)
+        fpr_keras, tpr_keras, thresholds_keras = roc_curve(
+            y_test, y_pred_keras)
         from sklearn.metrics import auc
         auc_keras = auc(fpr_keras, tpr_keras)
         print(auc_keras)
         return auc_keras
- 
+
     def score(self, X, y=None):
         # counts number of values bigger than mean
         print(" Score function is not implemented for FakeNN")
         return
-      
-      
-        
